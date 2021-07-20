346114c3c1938ce99e2de754a88e558139910257
jejoenje <jeroenminderman@gmail.com>
2021-07-20 15:00:42 +0100

    Updated JS tracker code

--- app/app.R
+++ app/app.R
@@ -129,21 +129,21 @@ ui <- fixedPage(
     tagList(tags$head(
         tags$style(".butt{font-family: Courier New;}"),
         HTML("
-            <!-- Matomo -->
-            <script type='text/javascript'>
-              var _paq = window._paq = window._paq || [];
-              _paq.push(['trackPageView']);
-              _paq.push(['enableLinkTracking']);
-              (function() {
-                var u='https://jejoenje.matomo.cloud/';
-                _paq.push(['setTrackerUrl', u+'matomo.php']);
-                _paq.push(['setSiteId', '2']);
-                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-                g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/jejoenje.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-              })();
-            </script>
-            <!-- End Matomo Code -->
-             ")
+          <!-- Matomo -->
+          <script type='text/javascript'>
+            var _paq = window._paq = window._paq || [];
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u='https://jejoenje.matomo.cloud/';
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '4']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/jejoenje.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          </script>
+          <!-- End Matomo Code -->
+        ")
     )),
     
     shinyjs::useShinyjs(),
