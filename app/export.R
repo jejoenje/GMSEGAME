54dce0d255e676656e3e8f9e0c61f3ae9018ae3c
jejoenje <jeroenminderman@gmail.com>
2021-08-17 12:20:52 +0100

    Added data export function

---
+++ app/export.R
@@ -0,0 +1,14 @@
+library(RMySQL)
+source("app/connect_db.R")
+
+db = connect_game_dbase()
+
+run = dbGetQuery(db, "SELECT * FROM run")
+run_par = dbGetQuery(db, "SELECT * FROM run_par")
+gdata = dbGetQuery(db, "SELECT * FROM gdata")
+scores = dbGetQuery(db, "SELECT * FROM scores") 
+
+write.csv(run, "~/af_run.csv")
+write.csv(run_par, "~/af_run_par.csv")
+write.csv(gdata, "~/af_run_gdata.csv")
+write.csv(scores, "~/af_run_scores.csv")
